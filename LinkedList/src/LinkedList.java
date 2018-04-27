70fbedbdd26ac8c49688b1ec7d575643b1113686
sepiggy <jms1209@qq.com>
2018-04-27 16:58:25 +0800

    B4 ch4-3 ok

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -22,4 +22,56 @@ public class LinkedList<E> {
             return e.toString();
         }
     }
+
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // 在链表头添加新的元素e
+    public void addFirst(E e) {
+//        Node node = new Node(e);
+//        node.next = head;
+//        head = node;
+
+        head = new Node(e, head);
+        size++;
+    }
+
+    // 在链表的index(0-based)位置添加新的元素e
+    public void add(int index, E e) {
+
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+
+        if (index == 0) {
+            addFirst(e);
+        } else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+//            Node node = new Node(e);
+//            node.next = prev.next;
+//            prev.next = node;
+
+            prev.next = new Node(e, prev.next);
+            size++;
+        }
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
 }
