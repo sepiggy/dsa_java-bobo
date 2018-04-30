89a2672743fa1fc3eefbecf41f27df98e4f2f2b3
sepiggy <jms1209@qq.com>
2018-04-30 09:42:20 +0800

    B4 ch5-3 ok

--- 203-Remove-Linked-List-Elements/src/Solution.java
+++ 203-Remove-Linked-List-Elements/src/Solution.java
@@ -23,4 +23,14 @@ public class Solution {
 
         return head;
     }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,6,3,4,5,6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new Solution()).removeElements(head, 6);
+        System.out.println(res);
+    }
 }
--- 203-Remove-Linked-List-Elements/src/Solution2.java
+++ 203-Remove-Linked-List-Elements/src/Solution2.java
@@ -21,4 +21,13 @@ public class Solution2 {
         return dummyHead.next;
     }
 
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 6, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new Solution2()).removeElements(head, 6);
+        System.out.println(res);
+    }
 }
