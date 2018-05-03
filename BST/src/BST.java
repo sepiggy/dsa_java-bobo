36b500d42d72babc0fcfb26a4c07cbc0a9b6076d
sepiggy <jms1209@qq.com>
2018-05-03 17:18:06 +0800

    B4 ch7-1 ok

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -249,6 +249,55 @@ public class BST<E extends Comparable<E>> {
         return node;
     }
 
+    // 从二分搜索树中删除元素为 e 的节点
+    public void remove(E e) {
+        root = remove(root, e);
+    }
+
+    // 删除掉以 node 为根的二分搜索树中值为 e 的节点, 递归算法
+    // 返回删除节点后新的二分搜索树的根
+    private Node remove(Node node, E e) {
+
+        if (node == null) {
+            return null;
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = remove(node.left, e);
+            return node;
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = remove(node.right, e);
+            return node;
+        } else { // e == node.e
+            // 待删除的节点左子树为空的情况
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+            // 待删除的节点右子树为空的情况
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+            // 待删除的节点左右子树均不为空的情况
+            // 找到比待删除节点大的最小节点, 即待删除节点右子书的最小节点
+            // 用这个节点顶替待删除节点的位置
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            size++;
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            size--;
+
+            return successor;
+        }
+    }
+
     // 前序遍历的应用
     @Override
     public String toString() {
