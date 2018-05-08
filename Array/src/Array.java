d97c34947a86ad26aca304d22a1c2e6663a30509
sepiggy <jms1209@qq.com>
2018-05-08 14:56:29 +0800

    B4 ch8-4 ok

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -154,6 +154,17 @@ public class Array<E> {
         return ret;
     }
 
+    public void swap(int i, int j) {
+
+        if (i < 0 || i >= size || j < 0 || j >= size) {
+            throw new IllegalArgumentException("Index is illegal.");
+        }
+
+        E t = data[i];
+        data[i] = data[j];
+        data[j] = t;
+    }
+
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
